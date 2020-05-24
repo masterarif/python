80ac77c4ae408b0b110e44e2d0df0b8dd50c26be
masterarif <marifk2000@googlemail.com>
2020-05-25 00:45:52 +0100

    python_variable and user input added

---
+++ python_variables.py
@@ -0,0 +1,18 @@
+
+
+# Python variables
+#
+# first_name = "Arif"
+# last_name = "khan"
+#
+# # Concatenate first and last name
+# full_name = first_name + " " + last_name
+#
+# # print full name
+# print(full_name)
+
+
+# # get user intput
+#
+user_name = input("What is your name ")
+print("Hi " + user_name)
